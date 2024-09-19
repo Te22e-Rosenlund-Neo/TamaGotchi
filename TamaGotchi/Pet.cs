22de5f0f69b22145b0e4118bbf543f91d609bc0d
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-09-19 10:22:36 +0200

    first commit

---
+++ TamaGotchi/Pet.cs
@@ -0,0 +1,69 @@
+﻿class Pet{
+
+    private int hunger = 0;
+    private int boredom = 0;
+    private List<string> knownWords;
+    private bool isAlive =  true;
+    public string name = "Default";
+    
+    public Pet(string name) {
+
+        this.name = name;
+
+    }  
+
+    public void Feed(){
+        hunger -= 2;
+        if(hunger < 0){
+            hunger = 0;
+        }
+    }
+    public void Hi(){
+        if(knownWords.Count == 0){
+            Console.WriteLine("Your friend doesnt know any words!");
+        }else{
+            Console.Write($"{name} says:    ");
+            Console.WriteLine(knownWords[Random.Shared.Next(0, knownWords.Count)]);
+            ReduceBoreDom();
+        }
+    }
+    public void Teach(string word){
+        knownWords.Add(word);
+
+    }
+    public void Tick(){
+
+        hunger += Random.Shared.Next(1,2);
+        boredom += Random.Shared.Next(0,2);
+
+        if(hunger == 10 || boredom == 10){
+            isAlive = false;
+            GetAlive();
+        }
+
+        Console.Clear();
+        Console.WriteLine("Time has passed");
+        Thread.Sleep(1500);
+        Console.Clear();
+    }
+    public void PrintStats(){
+        Console.WriteLine($"Tamagotch: {name}"); 
+        Console.WriteLine($"Boredom: {boredom}"); 
+        Console.WriteLine($"Hunger: {hunger}"); 
+    }
+    public void GetAlive(){
+        Console.WriteLine($"Your {name} is {isAlive}");
+    }
+    private void ReduceBoreDom(){
+         boredom -= 1;
+            if(boredom < 0){
+                boredom = 0;
+            }
+    }
+
+}
+
+
+
+
+
---
+++ TamaGotchi/Program.cs
@@ -0,0 +1,56 @@
+﻿
+//Improved tamagotchi where the user is too lazy to see be able to see the stats 
+// game does not end bc why would u want your tamagotchi to die? Meanie >:|
+
+
+
+
+Console.WriteLine("Welcome to tamagotchi");
+Console.WriteLine("Please type the name of your tamagotchi");
+Pet pet = new Pet(Console.ReadLine());
+Console.WriteLine(pet.name);
+Console.Clear();
+
+void Prompts(){
+Console.WriteLine("What do you want your pet to do?: ");
+Console.WriteLine("1:   Say a word");
+Console.WriteLine("2:   Eat");
+Console.WriteLine("3:   Teach a word");
+Console.WriteLine("4:   Show stats");
+Console.WriteLine("5:   Check alive");
+}
+void checkinput(string numb){
+    if(numb == "1"){
+        pet.Hi();
+    }
+    if(numb=="2"){
+        pet.Feed();
+    }
+    if(numb=="3"){
+        Console.WriteLine("what word");
+        string word = Console.ReadLine();
+    }
+    if(numb == "4"){
+       pet.PrintStats(); 
+    }
+    if(numb == "5"){
+        pet.GetAlive();
+    }
+
+}
+
+
+
+
+while(true){
+pet.Tick();
+Thread.Sleep(1500);
+Prompts();
+
+string input = Console.ReadLine();
+
+
+
+
+
+}
